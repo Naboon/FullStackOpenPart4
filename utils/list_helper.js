879c2f44158a708270cd37e4dfcf10b82cceed6e
Jesse Toivanen <jesse.toivanen2@gmail.com>
2022-03-06 18:39:59 +0200

    favoriteBlog test added, 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,12 +6,34 @@ const totalLikes = (blogs) => {
   const reducer = (sum, blog) => {
     return sum + blog.likes
   }
+
   return Object.keys(blogs).length === 0
     ? 0
     : blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (Object.keys(blogs).length === 0) {
+    return {}
+  }
+
+  const reducer = (max, blog) => {
+    return max.likes > blog.likes
+      ? max
+      : blog
+  }
+
+  let favoriteBlog = blogs.reduce(reducer)
+
+  return {
+    title: favoriteBlog.title,
+    author: favoriteBlog.author,
+    likes: favoriteBlog.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
