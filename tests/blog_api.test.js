2ab541886d42d1892d1342e5ae273d191ff42e84
Jesse Toivanen <jesse.toivanen2@gmail.com>
2022-03-16 22:18:53 +0200

    blog deletion and tests added, 4.13

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -11,99 +11,122 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('when there are initial blogs saved', () => {
+  test('blogs are returned as json with a status code 200', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('blogs have an identificator field called "id"', async () => {
+    const response = await api.get('/api/blogs')
+
+    const ids = response.body.map(r => r.id)
+
+    expect(ids).toBeDefined()
+  })
 })
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+describe('addition of a new blog', () => {
+  test('succeeds with status code 201', async () => {
+    const newBlog = {
+      title: 'How to Create a Blog',
+      author: 'Ricardo Gonzales',
+      url: 'http://techtoday.com/guides/blog',
+      likes: 1644
+    }
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(n => n.title)
+    expect(titles).toContain(
+      'How to Create a Blog'
+    )
+  })
+
+  test('without value for likes is assigned 0 likes', async () => {
+    const newBlog = {
+      title: 'A Lonely Person',
+      author: 'Alfred Kwak',
+      url: 'http://everydayproblems.com/blogs/loneliness',
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    expect(blogsAtEnd[blogsAtEnd.length - 1].likes).toEqual(0)
+  })
+
+  test('fails with status code 400 if title is not assigned', async () => {
+    const newBlog = {
+      author: 'Fred Goodman',
+      url: 'http://blogverse.com/blogs',
+      likes: 254
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('fails with status code 400 if url is not assigned', async () => {
+    const newBlog = {
+      title: 'Travelling Madman',
+      author: 'Saul Simpson',
+      likes: 775
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
 })
 
-test('blogs have an identificator field called "id"', async () => {
-  const response = await api.get('/api/blogs')
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogtoDelete = blogsAtStart[0]
 
-  const ids = response.body.map(r => r.id)
+    await api
+      .delete(`/api/blogs/${blogtoDelete.id}`)
+      .expect(204)
 
-  expect(ids).toBeDefined()
-})
+    const blogsAtEnd = await helper.blogsInDb()
 
-test('a blog can be added', async () => {
-  const newBlog = {
-    title: 'How to Create a Blog',
-    author: 'Ricardo Gonzales',
-    url: 'http://techtoday.com/guides/blog',
-    likes: 1644
-  }
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    const titles = blogsAtEnd.map(r => r.title)
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-  const titles = blogsAtEnd.map(n => n.title)
-  expect(titles).toContain(
-    'How to Create a Blog'
-  )
-})
-
-test('a blog with no likes value is assigned 0 likes', async () => {
-  const newBlog = {
-    title: 'A Lonely Person',
-    author: 'Alfred Kwak',
-    url: 'http://everydayproblems.com/blogs/loneliness',
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-  expect(blogsAtEnd[blogsAtEnd.length - 1].likes).toEqual(0)
-})
-
-test('a blog without title will not be added', async () => {
-  const newBlog = {
-    author: 'Fred Goodman',
-    url: 'http://blogverse.com/blogs',
-    likes: 254
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
-})
-
-test('a blog without url will not be added', async () => {
-  const newBlog = {
-    title: 'Travelling Madman',
-    author: 'Saul Simpson',
-    likes: 775
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    expect(titles).not.toContain(blogtoDelete.title)
+  })
 })
 
 afterAll(() => {
